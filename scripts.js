f3421891e12ff64ebd0f524072a4588244655e90
Dzemil <dzemilpapic5@gmail.com>
2023-06-06 16:12:18 +0200

    added coputerChoice function

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function getComputerChoice()
+{
+    const rockPaperScissors = ["Rock", "Paper", "Scissors"];
+
+    let randomChoice = Math.floor(Math.random() * rockPaperScissors.length);
+
+    return rockPaperScissors[randomChoice];
+}
+
+const computerChoice = getComputerChoice;
+
+console.log(computerChoice());
