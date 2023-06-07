a75375294d24756187ae4eb2920c5fe172a503d1
Dzemil <dzemilpapic5@gmail.com>
2023-06-07 11:32:17 +0200

    repeat 5 times

--- scripts.js
+++ scripts.js
@@ -7,57 +7,47 @@ function getComputerChoice()
     return rockPaperScissors[randomChoice];
 }
 
-function getUserChoice()
-{
-    return prompt();
-}
 
-const computerChoice = getComputerChoice().toLowerCase();
-const userChoice = getUserChoice().toLowerCase();
 
-function round(computerChoice, userChoice)
+for(let i = 0; i < 5; i++)
 {
-    if (computerChoice === userChoice)
-    {
-        return "It's a draw";
-    }
-    else if (computerChoice === "rock" && userChoice === "scissors")
-    {
-        return `You lose! ${computerChoice} beats ${userChoice}`;
-    }
-    else if (computerChoice === "rock" && userChoice === "paper")
-    {
-        return use`You win! ${userChoice} beats ${computerChoice}`;
-    }
-    else if (computerChoice === "paper" && userChoice === "rock")
-    {
-        return `You lose! ${computerChoice} beats ${userChoice}`;
-    }
-    else if (computerChoice === "paper" && userChoice === "scissors")
-    {
-        return use`You win! ${userChoice} beats ${computerChoice}`;
-    }
-    else if (computerChoice === "scissors" && userChoice === "rock")
-    {
-        return use`You win! ${userChoice} beats ${computerChoice}`;
-    }
-    else if (computerChoice === "scissors" && userChoice === "paper")
-    {
-        return `You lose! ${computerChoice} beats ${userChoice}`;
-    }
-    else
-    {
-        return "Check your imput";
-    }
-}
-console.log(computerScore, userScore);
+    const computerChoice = getComputerChoice().toLowerCase();
+    const userChoice = prompt().toLowerCase();
 
-/*
-function game()
-{
-    for (let i = 0; i < 5; i++)
+    function round(computerChoice, userChoice)
     {
-        round();
+        if (computerChoice === userChoice)
+        {
+            return "It's a draw";
+        }
+        else if (computerChoice === "rock" && userChoice === "scissors")
+        {
+            return `You lose! ${computerChoice} beats ${userChoice}`;
+        }
+        else if (computerChoice === "rock" && userChoice === "paper")
+        {
+            return `You win! ${userChoice} beats ${computerChoice}`;
+        }
+        else if (computerChoice === "paper" && userChoice === "rock")
+        {
+            return `You lose! ${computerChoice} beats ${userChoice}`;
+        }
+        else if (computerChoice === "paper" && userChoice === "scissors")
+        {
+            return `You win! ${userChoice} beats ${computerChoice}`;
+        }
+        else if (computerChoice === "scissors" && userChoice === "rock")
+        {
+            return `You win! ${userChoice} beats ${computerChoice}`;
+        }
+        else if (computerChoice === "scissors" && userChoice === "paper")
+        {
+            return `You lose! ${computerChoice} beats ${userChoice}`;
+        }
+        else
+        {
+            return "Check your imput";
+        }
     }
+    console.log(round(computerChoice,userChoice));
 }
-*/
