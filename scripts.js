75da0d0a86e715b11e7167de550cae49a6339d6c
Dzemil <dzemilpapic5@gmail.com>
2023-06-07 11:39:55 +0200

    code condensing

--- scripts.js
+++ scripts.js
@@ -20,29 +20,17 @@ for(let i = 0; i < 5; i++)
         {
             return "It's a draw";
         }
-        else if (computerChoice === "rock" && userChoice === "scissors")
+        else if ((computerChoice === "rock" && userChoice === "scissors") ||
+                 (computerChoice === "paper" && userChoice === "rock")    ||
+                 (computerChoice === "scissors" && userChoice === "paper"))
         {
             return `You lose! ${computerChoice} beats ${userChoice}`;
         }
-        else if (computerChoice === "rock" && userChoice === "paper")
+        else if ((computerChoice === "rock" && userChoice === "paper")     ||
+                 (computerChoice === "paper" && userChoice === "scissors") ||
+                 (computerChoice === "scissors" && userChoice === "rock"))
         {
             return `You win! ${userChoice} beats ${computerChoice}`;
-        }
-        else if (computerChoice === "paper" && userChoice === "rock")
-        {
-            return `You lose! ${computerChoice} beats ${userChoice}`;
-        }
-        else if (computerChoice === "paper" && userChoice === "scissors")
-        {
-            return `You win! ${userChoice} beats ${computerChoice}`;
-        }
-        else if (computerChoice === "scissors" && userChoice === "rock")
-        {
-            return `You win! ${userChoice} beats ${computerChoice}`;
-        }
-        else if (computerChoice === "scissors" && userChoice === "paper")
-        {
-            return `You lose! ${computerChoice} beats ${userChoice}`;
         }
         else
         {
